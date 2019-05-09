0d617f0173d8d9288568561bf8c2d67857f7c4ef
Jonathan Tomaney <jon.tomaney@gmail.com>
2019-05-09 07:42:41 +0100

    Pangram Uppercase

---
+++ models/pangram_finder.js
@@ -0,0 +1,15 @@
+const PangramFinder = function (string) {
+  this.string = string;
+  this.alphabet = [`q`, `w`, `e`, `r`, `t`, `y`, `u`, `i`, `o`, `p`, `a`, `s`, `d`, `f`, `g`, `h`, `j`, `k`, `l`, `z`, `x`, `c`, `v`, `b`, `n`, `m`]
+}
+
+// PangramFinder.prototype.compareLetter = function (letter) {
+//   this.string.include(letter);
+// }
+
+PangramFinder.prototype.isPangram = function () {
+  let result = this.alphabet.every(letter => this.string.toLowerCase().includes(letter))   
+  return result;
+};
+
+module.exports = PangramFinder;
