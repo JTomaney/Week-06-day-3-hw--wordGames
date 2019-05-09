017edccbf70937dd7de8fdaf4ac2c7231acb4649
Jonathan Tomaney <jon.tomaney@gmail.com>
2019-05-09 08:28:53 +0100

    Isogram detects true and false

---
+++ models/isogram_finder.js
@@ -0,0 +1,21 @@
+const IsogramFinder = function (string) {
+  this.string = string
+}
+
+IsogramFinder.prototype.isIsogram = function () {
+  const timesLetterUsed = {};
+  const arrayOfLetters = this.string.split("")
+  arrayOfLetters.forEach(function (letter) {
+
+  if (timesLetterUsed[letter]) {
+    timesLetterUsed[letter] += 1
+  } else {
+    timesLetterUsed[letter] = 1
+  }
+})
+  const result = arrayOfLetters.every(letter => timesLetterUsed[letter] <= 1);
+  return result;
+};
+
+
+module.exports = IsogramFinder;
